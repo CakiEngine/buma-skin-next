cdeafd1b1468b5d8357af803350f8268dea3f702
Duswnsey <157892378+Duswnsey@users.noreply.github.com>
2025-05-05 18:22:10 +0900

    잡다

--- components/bulma/b-heroBody.js
+++ components/bulma/b-heroBody.js
@@ -1,16 +1,14 @@
 export default function BHeroBody(props) {
     return (
-        <>
-            <div className="hero-body">
-                <div className="container">
-                    <h1 className="title">
-                        {props.title}
-                    </h1>
-                    <h2 className="subtitle">
-                        {props.subtitle}
-                    </h2>
-                </div>
+        <div className="hero-body">
+            <div className="container">
+                <h1 className="title">
+                    {props.title}
+                </h1>
+                <h2 className="subtitle">
+                    {props.subtitle}
+                </h2>
             </div>
-        </>
+        </div>
     )
 }
